0650e1a41219f2d1dca84d6b97bf60a38f3d6652
Mohamed Saleh <msaleh@evero.net>
2020-12-11 14:11:30 -0500

    added-app-header-component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
-    App Component
+   <AppHeader></AppHeader>
+  
   </div>
 </template>
 
 <script>
 
-
+import AppHeader from './components/AppHeader.vue'
 export default {
   name: 'App',
   components: {
-    
+    AppHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        App Header 
+    </div>
+</template>
+<script>
+export default {
+    name:'AppHeader'
+}
+</script>
+<style scoped>
+
+</style>
